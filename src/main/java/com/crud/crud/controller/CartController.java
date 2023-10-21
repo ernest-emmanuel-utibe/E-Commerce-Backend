08e36cd8b5e22f2056ff531f0fe5e4450bf39424
ernest-emmanuel-utibe <em123nest@gmail.com>
2023-10-21 17:00:20 +0100

    Starting project

---
+++ src/main/java/com/crud/crud/controller/CartController.java
@@ -0,0 +1,57 @@
+package com.crud.crud.controller;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+public class CartController {
+//    @Autowired
+    private final CartService cartService;
+
+//    @Autowired
+    private final CartDao cartDao;
+
+    @Autowired
+    private CustomerDao customerDao;
+
+
+    @PostMapping(value = "/cart/add")
+    public ResponseEntity<Cart> addProductToCartHander(@RequestBody CartDTO cartdto ,@RequestHeader("token")String token){
+
+        Cart cart = cartService.addProductToCart(cartdto, token);
+        return new ResponseEntity<Cart>(cart,HttpStatus.CREATED);
+    }
+
+    //
+    @GetMapping(value = "/cart")
+    public ResponseEntity<Cart> getCartProductHandler(@RequestHeader("token")String token){
+        return new ResponseEntity<>(cartService.getCartProduct(token), HttpStatus.ACCEPTED);
+    }
+
+
+    @DeleteMapping(value = "/cart")
+    public ResponseEntity<Cart> removeProductFromCartHander(@RequestBody CartDTO cartdto ,@RequestHeader("token")String token){
+
+        Cart cart = cartService.removeProductFromCart(cartdto, token);
+        return new ResponseEntity<Cart>(cart,HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(value = "/cart/clear")
+    public ResponseEntity<Cart> clearCartHandler(@RequestHeader("token") String token){
+        return new ResponseEntity<>(cartService.clearCart(token), HttpStatus.ACCEPTED);
+    }
+
+}
